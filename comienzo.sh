b2b2e11dbd05e17416b84e32e2e0d6cfb65450b1
1OSAMABINLADEN1 <95018384+1OSAMABINLADEN1@users.noreply.github.com>
2021-11-27 14:59:37 +0530

    Update comienzo.sh

--- comienzo.sh
+++ comienzo.sh
@@ -1,1 +1,5 @@
+if [[ -n $TOKEN_PICKLE_URL ]]; then
+ wget -q $TOKEN_PICKLE_URL -O /usr/src/app/token.pickle
+fi
+
 gunicorn wserver:start_server --bind 0.0.0.0:$PORT --worker-class aiohttp.GunicornWebWorker & qbittorrent-nox -d --webui-port=8090 & python3 alive.py & ./aria.sh; python3 -m bot
